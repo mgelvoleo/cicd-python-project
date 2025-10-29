e8551345638c63b2a85054a0f31fad66f5956c0f
mgelvoleo <mgelvoleo@gmail.com>
2025-10-29 21:35:36 +0800

    fix the error from the apps.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,7 +7,7 @@ class TestApp(unittest.TestCase):
     def test_hello_route(self):
         client = app.test_client()
         res = client.get("/")
-        
+
         # Replace assert with unittest assertions
         self.assertEqual(res.status_code, 200)
         self.assertIn(b"Hello, Kubernetes CI/CD", res.data)
